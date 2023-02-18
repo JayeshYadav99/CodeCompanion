cd0f50be18164edbc9e307a8734c1ef12d1caf2f
JayeshYadav99 <jayeshyadav528@gmail.com>
2023-02-18 19:02:59 +0530

    codex

--- client/script.js
+++ client/script.js
@@ -23,7 +23,7 @@ const timebtn = document.getElementById('time');
 if(timebtn)
 {
   time.addEventListener('click', function (e) {
-    salt = "find  the time and space complexity of the given code each with proper explanation";
+    salt = "What is the time and space complexity of the given code?explain each with proper explanation";
     
     handleSubmit(e, salt);
   });
